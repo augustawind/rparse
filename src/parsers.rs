fb49df1199f9acec2cc6e1279ed53c42b8dadf2d
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-20 23:40:37 -0400

    reorder code

--- src/parsers.rs
+++ src/parsers.rs
@@ -3,6 +3,24 @@ use std::marker::PhantomData;
 
 use parser::{Error, Info, Input, ParseResult, Parser};
 
+pub struct Any<I: Input>(PhantomData<I>);
+
+impl<I: Input> Parser for Any<I> {
+    type Input = I;
+    type Output = I::Item;
+
+    fn parse(&mut self, mut input: Self::Input) -> ParseResult<Self::Input, Self::Output> {
+        match input.pop() {
+            Some(t) => input.ok(t),
+            _ => input.err(Error::Expected(Info::AnyToken)),
+        }
+    }
+}
+
+pub fn any<I: Input>() -> Any<I> {
+    Any(PhantomData)
+}
+
 #[derive(Copy, Clone)]
 pub struct Token<I: Input>(I::Item);
 
@@ -28,22 +46,32 @@ pub fn token<I: Input>(item: I::Item) -> Token<I> {
     Token(item)
 }
 
-pub struct Any<I: Input>(PhantomData<I>);
+pub struct TokenCond<I: Input, F>(F, PhantomData<Fn(&I::Item) -> bool>);
 
-impl<I: Input> Parser for Any<I> {
+impl<I: Input, F> Parser for TokenCond<I, F>
+where
+    F: Fn(&I::Item) -> bool,
+{
     type Input = I;
     type Output = I::Item;
 
     fn parse(&mut self, mut input: Self::Input) -> ParseResult<Self::Input, Self::Output> {
-        match input.pop() {
-            Some(t) => input.ok(t),
-            _ => input.err(Error::Expected(Info::AnyToken)),
+        match input.peek() {
+            Some(ref t) if self.0(t) => {
+                input.pop();
+                input.ok(*t)
+            }
+            Some(ref t) => input.err(Error::Unexpected(Info::Token(*t))),
+            None => input.err(Error::Expected(Info::AnyToken)),
         }
     }
 }
 
-pub fn any<I: Input>() -> Any<I> {
-    Any(PhantomData)
+pub fn token_if<I: Input, F>(f: F) -> TokenCond<I, F>
+where
+    F: Fn(&I::Item) -> bool,
+{
+    TokenCond(f, PhantomData)
 }
 
 #[derive(Copy, Clone)]
@@ -91,49 +119,21 @@ where
     }
 }
 
-pub struct TokenCond<I: Input, F>(F, PhantomData<Fn(&I::Item) -> bool>);
-
-impl<I: Input, F> Parser for TokenCond<I, F>
-where
-    F: Fn(&I::Item) -> bool,
-{
-    type Input = I;
-    type Output = I::Item;
-
-    fn parse(&mut self, mut input: Self::Input) -> ParseResult<Self::Input, Self::Output> {
-        match input.peek() {
-            Some(ref t) if self.0(t) => {
-                input.pop();
-                input.ok(*t)
-            }
-            Some(ref t) => input.err(Error::Unexpected(Info::Token(*t))),
-            None => input.err(Error::Expected(Info::AnyToken)),
-        }
-    }
-}
-
-pub fn token_if<I: Input, F>(f: F) -> TokenCond<I, F>
-where
-    F: Fn(&I::Item) -> bool,
-{
-    TokenCond(f, PhantomData)
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn test_any() {
+        let input = "hello, world.";
+        assert_eq!(any().parse(input), (Ok('h'), "ello, world."));
+    }
 
     #[test]
     fn test_token() {
         let mut parser = token('c');
         assert_eq!(parser.parse("cat"), (Ok('c'), "at"));
         assert_eq!(parser.parse("ace").1, "ace");
-    }
-
-    #[test]
-    fn test_any() {
-        let input = "hello, world.";
-        assert_eq!(any().parse(input), (Ok('h'), "ello, world."));
     }
 
     #[test]
