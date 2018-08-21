6ffd853792667c3304c2cc2949ffa908784b9b2c
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-20 21:45:21 -0400

    impl many parser

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,3 +1,4 @@
+use std::iter::FromIterator;
 use std::marker::PhantomData;
 
 use parser::{Error, Input, ParseResult, Parser};
@@ -46,33 +47,47 @@ pub fn any<I: Input>() -> Any<I> {
 }
 
 #[derive(Copy, Clone)]
-pub struct Many<P> {
+pub struct Many<P, O> {
     p: P,
     min: usize,
     max: Option<usize>,
+    _marker: PhantomData<O>,
 }
 
-// impl<P> Parser for Many<P>
-// where
-//     P: Parser,
-// {
-//     type Input = P::Input;
-//     type Output = Vec<P::Output>;
-
-//     fn parse(&mut self, mut i: Self::Input) -> ParseResult<Self::Input, Self::Output> {
-//         let p = self.p.parse(i);
-//         // TODO
-//     }
-// }
-
-pub fn many<P>(p: P) -> Many<P>
+impl<P, O> Parser for Many<P, O>
 where
     P: Parser,
+    O: FromIterator<P::Output>,
+{
+    type Input = P::Input;
+    type Output = O;
+
+    fn parse(&mut self, mut input: Self::Input) -> ParseResult<Self::Input, Self::Output> {
+        let mut output = Vec::new();
+        loop {
+            match self.p.parse(input) {
+                (Ok(result), remaining) => {
+                    output.push(result);
+                    input = remaining;
+                }
+                (Err(_), remaining) => {
+                    return (Ok(output.into_iter().collect()), remaining);
+                }
+            }
+        }
+    }
+}
+
+pub fn many<P, O>(p: P) -> Many<P, O>
+where
+    P: Parser,
+    O: FromIterator<P::Output>,
 {
     Many {
         p,
         min: 0,
         max: None,
+        _marker: PhantomData,
     }
 }
 
@@ -131,6 +146,15 @@ mod test {
         assert_eq!(
             token_if(|&c: &char| c.is_alphabetic()).parse(input).1,
             input
+        );
+    }
+
+    #[test]
+    fn test_many() {
+        let input = "aaabcdef";
+        assert_eq!(
+            many(token('a')).parse(input),
+            (Ok("aaa".to_string()), "bcdef")
         );
     }
 }
