0857939b8d461cee130dec5ce02f434de158e388
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-21 00:52:11 -0400

    mv cond to parsers/token

--- src/parsers/token.rs
+++ src/parsers/token.rs
@@ -45,6 +45,34 @@ pub fn token<I: Input>(item: I::Item) -> Token<I> {
     Token(item)
 }
 
+pub struct Cond<I: Input, F>(F, PhantomData<Fn(&I::Item) -> bool>);
+
+impl<I: Input, F> Parser for Cond<I, F>
+where
+    F: Fn(&I::Item) -> bool,
+{
+    type Input = I;
+    type Output = I::Item;
+
+    fn parse(&mut self, mut input: Self::Input) -> ParseResult<Self::Input, Self::Output> {
+        match input.peek() {
+            Some(ref t) if self.0(t) => {
+                input.pop();
+                input.ok(*t)
+            }
+            Some(ref t) => input.err(Error::Unexpected(Info::Token(*t))),
+            None => input.err(Error::Expected(Info::AnyToken)),
+        }
+    }
+}
+
+pub fn cond<I: Input, F>(f: F) -> Cond<I, F>
+where
+    F: Fn(&I::Item) -> bool,
+{
+    Cond(f, PhantomData)
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -60,5 +88,16 @@ mod test {
         let mut parser = token('c');
         assert_eq!(parser.parse("cat"), (Ok('c'), "at"));
         assert_eq!(parser.parse("ace").1, "ace");
+    }
+
+    #[test]
+    fn test_cond() {
+        let input = "123abc";
+        assert_eq!(
+            cond(|&c: &char| c.is_numeric()).parse(input),
+            (Ok('1'), "23abc")
+        );
+        let input = "123abc";
+        assert_eq!(cond(|&c: &char| c.is_alphabetic()).parse(input).1, input);
     }
 }
