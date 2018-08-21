68faec088fe922a51e311b8e5fc65997b3b79489
Dustin Rohde <dustin.rohde@gmail.com>
2018-08-21 13:08:12 -0400

    add map parser

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,6 +1,7 @@
 pub mod choice;
 pub mod many;
+pub mod map;
 pub mod sep_by;
 pub mod token;
 
-pub use parsers::{choice::*, many::*, sep_by::*, token::*};
+pub use parsers::{choice::*, many::*, map::*, sep_by::*, token::*};
