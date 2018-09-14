5dfea059bc3047228012dc1e42357b83c8ab1294
Dustin Rohde <dustin.rohde@gmail.com>
2018-09-13 20:29:20 -0400

    impl Stream for &[u8]

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -194,6 +194,72 @@ impl<'a> ToStream<&'a str> for char {
     }
 }
 
+impl<'a> Stream for &'a [u8] {
+    type Item = u8;
+    type Range = Self;
+    type Position = NullPosition;
+
+    fn from_token(token: Self::Item) -> Self {
+        token.to_stream()
+    }
+
+    fn from_range(range: Self::Range) -> Self {
+        range
+    }
+
+    fn peek(&self) -> Option<Self::Item> {
+        self.first().map(|t| *t)
+    }
+
+    fn pop(&mut self) -> Option<Self::Item> {
+        self.split_first().map(|(head, tail)| {
+            *self = &tail;
+            *head
+        })
+    }
+
+    fn tokens(&self) -> Tokens<Self::Item> {
+        Tokens::new(self.iter().map(|t| *t))
+    }
+
+    fn range(&mut self, idx: usize) -> Option<Self::Range> {
+        if idx > self.len() {
+            None
+        } else {
+            let (head, tail) = self.split_at(idx);
+            *self = &tail;
+            Some(head)
+        }
+    }
+
+    fn position(&self) -> Self::Position {
+        NullPosition(())
+    }
+}
+
+impl<'a> RangeStream for &'a [u8] {
+    fn len(&self) -> usize {
+        <[u8]>::len(self)
+    }
+}
+
+impl<'a> ToStream<Self> for &'a [u8] {
+    fn to_stream(self) -> Self {
+        self
+    }
+}
+
+impl<'a> ToStream<&'a [u8]> for u8 {
+    fn to_stream(self) -> &'a [u8] {
+        let s = vec![self];
+        unsafe {
+            let stream: &'a [u8] = mem::transmute_copy(&s.as_slice());
+            mem::forget(s);
+            stream
+        }
+    }
+}
+
 impl Stream for String {
     type Item = char;
     type Range = Self;
