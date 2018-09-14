f003caba893d5e939749cdec8ecbe972fb255884
Dustin Rohde <dustin.rohde@gmail.com>
2018-09-14 19:11:52 -0400

    impl Extend parser

---
+++ src/parser/combinator/extend.rs
@@ -0,0 +1,76 @@
+use std::iter;
+use std::marker::PhantomData;
+
+use error::ParseResult;
+use parser::Parser;
+use stream::Stream;
+
+pub struct Extend<L, R, E, I> {
+    left: L,
+    right: R,
+    __marker: PhantomData<(E, I)>,
+}
+
+impl<S: Stream, E, I, O, L, R> Parser for Extend<L, R, E, I>
+where
+    L: Parser<Stream = S, Output = E>,
+    R: Parser<Stream = S, Output = I>,
+    E: iter::Extend<O>,
+    I: IntoIterator<Item = O>,
+{
+    type Stream = S;
+    type Output = E;
+
+    fn parse_stream(&mut self, stream: Self::Stream) -> ParseResult<Self::Stream, Self::Output> {
+        match self.left.parse_stream(stream) {
+            (Ok(mut left), stream) => match self.right.parse_stream(stream) {
+                (Ok(right), stream) => {
+                    left.extend(right.into_iter());
+                    stream.ok(left)
+                }
+                (Err(err), stream) => stream.errs(err),
+            },
+            (Err(err), stream) => stream.errs(err),
+        }
+    }
+}
+
+pub fn extend<S: Stream, E, I, O, L, R>(left: L, right: R) -> Extend<L, R, E, I>
+where
+    L: Parser<Stream = S, Output = E>,
+    R: Parser<Stream = S, Output = I>,
+    E: iter::Extend<O>,
+    I: IntoIterator<Item = O>,
+{
+    Extend {
+        left,
+        right,
+        __marker: PhantomData,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use error::Error;
+    use parser::combinator::{many, many1};
+    use parser::token::ascii::letter;
+    use parser::token::token;
+    use parser::Parser;
+    use stream::IndexedStream;
+
+    #[test]
+    fn test_extend() {
+        let mut parser = extend::<_, String, Vec<char>, _, _, _>(many(letter()), many1(token('?')));
+        test_parser!(IndexedStream<&str> | parser, {
+            "huh???" => (Ok("huh???".to_string()), "", 6);
+            "oh?? cool" => (Ok("oh??".to_string()), " cool", 4);
+            "???" => (Ok("???".to_string()), "", 3);
+        });
+        test_parser_errors!(IndexedStream<&str> | parser, {
+            "" => at 0; vec![Error::EOF, Error::expected_token('?')];
+            "whoops!" => at 6; vec![Error::unexpected_token('!'), Error::expected_token('?')];
+            "!?" => at 0; vec![Error::unexpected_token('!'), Error::expected_token('?')];
+        });
+    }
+}
--- src/parser/combinator/mod.rs
+++ src/parser/combinator/mod.rs
@@ -1,10 +1,12 @@
 //! Parsers that combine other parsers.
 
 pub mod append;
+pub mod extend;
 pub mod many;
 #[macro_use]
 pub mod then;
 
 pub use self::append::*;
+pub use self::extend::*;
 pub use self::many::*;
 pub use self::then::*;
